d54384daf835d9b3328575047943a3c718cb9bf2
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-12-25 23:31:18 +0300

    project creation

---
+++ src/test/java/com/bbd/oddsshuffle/OddsShuffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbd.oddsshuffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddsShuffleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
