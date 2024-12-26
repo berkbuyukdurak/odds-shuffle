c56da9b865fadb5b6cb249f7fefc3350ae260743
berkbuyukdurak <berkbuyukdurak07@gmail.com>
2024-12-26 13:18:05 +0300

    scheduling enabled.

--- src/main/java/com/bbd/oddsshuffle/OddsShuffleApplication.java
+++ src/main/java/com/bbd/oddsshuffle/OddsShuffleApplication.java
@@ -2,8 +2,10 @@ package com.bbd.oddsshuffle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OddsShuffleApplication {
 
     public static void main(String[] args) {
